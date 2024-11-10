9a37ebfb0b22acc163a758fd8c991ade75b7006c
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-11-10 17:04:59 -0500

    Rutas y CORS

---
+++ src/gpt/gpt.controller.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Post } from '@nestjs/common';
+import { GptService } from './gpt.service';
+
+@Controller('gpt')
+export class GptController {
+  constructor(private readonly gptService: GptService) {}
+
+  @Post('orthography-check')
+  orthographyCheck() {
+    return this.gptService.orthographyCheck();
+  }
+}
